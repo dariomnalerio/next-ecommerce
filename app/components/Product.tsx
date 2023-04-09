f7fdd84ca702e689750ad257d3cdb9ee5c2d7da0
dariomnalerio <dariomnalerio@gmail.com>
2023-04-09 20:00:19 -0300

    Add price formatter

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image"
+import formatPrice from "@/util/PriceFormat"
+import { ProductType } from "@/types/ProductType"
 
-export default function Product({name, image, price}) {
+export default function Product({name, image, price,}: ProductType) {
     return (
         <div>
             <Image src={image} alt={name} width={400} height={400} />
             <h1>{name}</h1>
-            {price}
+            {/* If price is null, display N/A, else display price */}
+            {price !== null ? formatPrice(price) : 'N/A'}
         </div>
     )
 }
---
+++ util/PriceFormat.tsx
@@ -0,0 +1,10 @@
+import { format } from "path"
+
+const formatPrice = (amount: number) => {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+}).format(amount / 100)
+}
+
+export default formatPrice
